be63a49ce61669f77a4af36362785252f3d3e237
Hunter Hudson <hunterhudson3@gmail.com>
2020-10-22 13:16:33 -0600

    Add CoreData code to AppDelegate, change container name to “Model,” create new entity in CoreData “Item,” add attribute “title” of type non-optional String and “done” of type non-optional boolean, comment out ListItem class as it is no longer necessary due to CoreData, change entity module to Current Product Module to prevent errors with CoreData.

--- Todoey/Model/ListItem.swift
+++ Todoey/Model/ListItem.swift
@@ -6,11 +6,16 @@
 //  Copyright © 2020 App Brewery. All rights reserved.
 //
 
+//NOTE: this file is mostly commented out because it was used as an example for how to
+//use UserDefaults and .plist encoding:
+
+
 import Foundation
 
 /*Creates a data type for each item in the list (conforms to Codable so it can be coded and decoded into the database). REMEMBER: in order to conform to Codable (or Encodable or Decodable), all properties must be standard data types (i.e. Int and String) and not custom data types that you, the coder, made up:
-*/
+
 class ListItem: Codable{
     var title: String = ""
     var done: Bool = false
 }
+*/
